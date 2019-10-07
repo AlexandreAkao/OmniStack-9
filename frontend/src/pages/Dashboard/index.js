7617855e0273be68686845feef4d573117ed02de
Alexandre Akira <alexandreakiraakao@gmail.com>
2019-10-07 03:14:49 -0300

    estilização do dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import api from '../../services/api';
+
+import './styles.css';
 
 export default function Dashboard() {
     const [spots, setSpots] = useState([]);
@@ -11,10 +14,9 @@ export default function Dashboard() {
             const resp = await api.get('/dashboard', {
                 headers: { user_id }
             });
-
             setSpots(resp.data);
         }
-
+        
         loadSpots();
     }, []);
 
@@ -23,12 +25,18 @@ export default function Dashboard() {
             <ul className="spot-list">
                 {spots.map(spot => (
                    <li key={spot._id}>
-                        <header />
+                        <header style={{
+                            backgroundImage: `url(${spot.thumbnail_url})`
+                        }}/>
                         <strong>{spot.company}</strong>
-                        <span>{spot.price}</span>
+                        <span>{spot.price ? `R$${spot.price}/dia` : "GRATUITO"}</span>
                    </li>
                 ))}
             </ul>
+            
+            < Link to="/new">
+                <button className="btn">Cadastrar novo Spot</button>
+            </Link>
         </>
     );
 }
