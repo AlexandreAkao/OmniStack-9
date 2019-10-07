93c4fffeea4e820483e1ba37171297bf99709ec8
Alexandre Akira <alexandreakiraakao@gmail.com>
2019-10-07 00:09:04 -0300

    bookingController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,7 @@ const uploadConfig = require("./config/upload");
 const SessionController = require("./controllers/SessionController");
 const SpotController = require("./controllers/SpotController");
 const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
@@ -14,7 +15,9 @@ routes.post("/sessions", SessionController.store);
 
 routes.get("/spots", SpotController.index);
 routes.post("/spots", upload.single("thumbnail"), SpotController.store);
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
 routes.get("/dashboard", DashboardController.show);
+
 
 module.exports = routes;
