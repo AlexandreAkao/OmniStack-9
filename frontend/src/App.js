fefe33dcf1815c0c5991dfaf6d9198ad2329233f
Alexandre Akira <alexandreakiraakao@gmail.com>
2019-10-07 01:56:20 -0300

    utilizando axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
+
+import api from './services/api';
+
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        console.log("hello World");
+    }
+
+
     return (
         <div className="container">
-            
+            <img src={logo} alt="Aircnc"/>
+
+            <div className="content">
+                <p>
+                    Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+                </p>
+
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email"> E-MAIL *</label>
+                    <input 
+                        id="email"
+                        type="email"
+                        placeholder="Seu melhor e-mail"
+                    />
+
+                    <button className="btn" type="submit">Entrar</button>
+                </form>
+            </div>
         </div>
     );
 }
